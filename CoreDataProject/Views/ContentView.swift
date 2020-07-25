7007520787292907da2744af816ee47a8f24330c
Nate Lee <nateleee.dev@gmail.com>
2020-07-25 10:20:39 +0800

    - [] NSPredicate - 1.

--- CoreDataProject/Views/ContentView.swift
+++ CoreDataProject/Views/ContentView.swift
@@ -12,36 +12,37 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(entity: Wizard.entity(), sortDescriptors: []) var wizards: FetchedResults<Wizard>
+    @FetchRequest(entity: Ship.entity(), sortDescriptors: [], predicate: NSPredicate(format: "universe == %@", "Star Wars")) var ships: FetchedResults<Ship>
     
     var body: some View {
         VStack {
-            List {
-                ForEach(wizards, id: \.self) { wizard in
-                    Text(wizard.name ?? "Unknown Wizard")
+            VStack(spacing: 9) {
+                List {
+                    ForEach(ships, id: \.self) { ship in
+                        Text(ship.name ?? "Unknown Ship")
+                    }
+                }
+                
+                Button("Add Examples") {
+                    let ship1 = Ship(context: self.moc)
+                    ship1.name = "Enterprise"
+                    ship1.universe = "Star Trek"
+
+                    let ship2 = Ship(context: self.moc)
+                    ship2.name = "Defiant"
+                    ship2.universe = "Star Trek"
+
+                    let ship3 = Ship(context: self.moc)
+                    ship3.name = "Millennium Falcon"
+                    ship3.universe = "Star Wars"
+
+                    let ship4 = Ship(context: self.moc)
+                    ship4.name = "Executor"
+                    ship4.universe = "Star Wars"
+
+                    try? self.moc.save()
                 }
             }
-            
-            
-            Button(action: {
-                let newWizard = Wizard(context: self.moc)
-                newWizard.name = "Jack"
-                // self.moc.insert(newWizard) // Is this neccesary?
-            }) {
-                Text("Add")
-            }
-            .padding(.bottom, 27)
-            
-            Button(action: {
-                do {
-                    try self.moc.save()
-                } catch let error {
-                    print(error.localizedDescription)
-                }
-            }) {
-                Text("Save")
-            }
-            .padding(.bottom, 27)
         }
     }
 }

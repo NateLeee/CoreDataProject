c175c18a3739a955dc018cf259683ddd11073b31
Nate Lee <nateleee.dev@gmail.com>
2020-07-26 19:43:49 +0800

    Added a new enum: Predicate.

---
+++ CoreDataProject/Enums/Predicate.swift
@@ -0,0 +1,13 @@
+//
+//  Predicate.swift
+//  CoreDataProject
+//
+//  Created by Nate Lee on 7/26/20.
+//  Copyright © 2020 Nate Lee. All rights reserved.
+//
+
+import Foundation
+
+enum Predicate {
+    case beginsWith
+}
